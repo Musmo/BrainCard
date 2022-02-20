2f0efce59a9a5cb6e9080c7a9b84f891a2c09c9f
Musmo <antonino.musmeci@gmail.com>
2022-02-20 18:34:40 +0100

    Initial Setup

---
+++ braincard/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import Home from './features/home/Home';
+
+
+function App() {
+  return (
+      <Home></Home>
+  );
+}
+
+export default App;
